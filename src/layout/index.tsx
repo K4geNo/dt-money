973a449e488839434f4437d5e7bf06937d4e423c
K4geNo <micaraujo71@gmail.com>
2023-02-10 18:00:04 -0300

    Style(Layout): Change background color

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -9,7 +9,7 @@ export function Layout({ children }: LayoutProps) {
     const { asPath } = useRouter()
 
     return (
-        <main className="flex h-screen w-screen bg-zinc-800 text-white">
+        <main className="flex h-screen w-screen bg-[#202024] text-white">
             <div className="flex w-full flex-col items-center">
                 {asPath !== '/' && <Header />}
                 <div className="mx-auto flex w-full max-w-6xl flex-col">
